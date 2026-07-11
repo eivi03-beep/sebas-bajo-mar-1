959306283ac9f77c98ca136c3c52d137d960b274
eivi03-beep <eivi_03@hotmail.com>
2026-07-10 22:51:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Aquí agregaremos la interacción de la invitación
+const boton = document.getElementById("entrar");
+
+const olas = document.getElementById("sonidoOlas");
+const musica = document.getElementById("musica");
+
+
+boton.addEventListener("click", function(){
+
+    olas.volume = 0.4;
+    musica.volume = 0.3;
+
+    olas.play();
+    musica.play();
+
+    document.querySelector(".bienvenida").scrollIntoView({
+        behavior:"smooth"
+    });
+
+});
