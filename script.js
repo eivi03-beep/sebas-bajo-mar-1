0eb59a37ef3194e07615cd11203b136c88a0d75f
eivi03-beep <eivi_03@hotmail.com>
2026-07-10 23:50:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,26 @@ boton.addEventListener("click", function(){
     });
 
 });
+// CONTADOR REGRESIVO
+
+const fechaEvento = new Date("August 25, 2026 15:00:00").getTime();
+
+const contador = setInterval(function(){
+
+    const ahora = new Date().getTime();
+
+    const distancia = fechaEvento - ahora;
+
+    const dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((distancia % (1000 * 60)) / 1000);
+
+
+    document.getElementById("dias").innerHTML = dias;
+    document.getElementById("horas").innerHTML = horas;
+    document.getElementById("minutos").innerHTML = minutos;
+    document.getElementById("segundos").innerHTML = segundos;
+
+
+},1000);
